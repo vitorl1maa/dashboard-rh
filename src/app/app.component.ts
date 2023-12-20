048f251bec8066c0ba4b889ab9d9d47366047caa
vitorl1maa <vitornascimento923@gmail.com>
2023-12-19 21:16:41 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { StatusBarComponent } from './components/status-bar/status-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, StatusBarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'dashboard-rh';
---
+++ src/app/components/status-bar/status-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-status-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './status-bar.component.html',
+  styleUrl: './status-bar.component.css'
+})
+export class StatusBarComponent {
+
+}
