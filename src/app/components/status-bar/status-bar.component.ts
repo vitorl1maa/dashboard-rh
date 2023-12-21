f50a030d361de58aad95edc6bcbd0d53d3023e0b
vitorl1maa <vitornascimento923@gmail.com>
2023-12-20 22:08:57 -0300

    statusbar

--- src/app/components/status-bar/status-bar.component.ts
+++ src/app/components/status-bar/status-bar.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import {
+  NgIconComponent,
+  provideIcons,
+  provideNgIconsConfig,
+} from '@ng-icons/core';
+import {
+  bootstrapPerson,
+  bootstrapClipboardCheck,
+  bootstrapCloudyFill,
+  bootstrapHddRackFill,
+  bootstrapExclamationOctagon,
+} from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'app-status-bar',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
   templateUrl: './status-bar.component.html',
-  styleUrl: './status-bar.component.css'
+  styleUrl: './status-bar.component.css',
+  viewProviders: [
+    provideIcons({
+      bootstrapPerson,
+      bootstrapClipboardCheck,
+      bootstrapCloudyFill,
+      bootstrapHddRackFill,
+      bootstrapExclamationOctagon,
+    }),
+  ],
+  providers: [
+    provideNgIconsConfig({
+      size: '2rem',
+      color: '#ffff',
+    }),
+  ],
 })
-export class StatusBarComponent {
-
-}
+export class StatusBarComponent {}
